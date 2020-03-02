abc91a13d9aea6c2aeb292f285f6de8e43e72363
Carlos <cafubu@gmail.com>
2020-03-02 19:29:36 +0100

    Initial Send and receive programs
    Working with touch/release commands

---
+++ receive/receive.ino
@@ -0,0 +1,65 @@
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <SPI.h>
+#include <MIDI.h>
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+const int pinCE = 9;
+const int pinCSN = 10;
+RF24 radio(pinCE, pinCSN);
+ 
+const uint64_t talking_pipes[5] = { 0xF0F0F0F0D2LL, 0xF0F0F0F0C3LL, 0xF0F0F0F0B4LL, 0xF0F0F0F0A5LL, 0xF0F0F0F096LL };
+
+const uint8_t bufferSize = 6;
+uint8_t data[bufferSize];
+
+const uint8_t notes[12] = {59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48};
+
+void setup(void) {
+   MIDI.begin();
+   Serial.begin(115200);
+   radio.begin();
+   radio.openReadingPipe(1,talking_pipes[0]);
+   radio.openReadingPipe(2,talking_pipes[1]);
+   radio.openReadingPipe(3,talking_pipes[2]);
+   radio.openReadingPipe(4,talking_pipes[3]);
+   radio.openReadingPipe(5,talking_pipes[4]);
+   radio.startListening();
+}
+
+void show(uint8_t data[]) {
+  Serial.print("[0]=");
+  Serial.println(data[0]);
+  Serial.print("[1]=");
+  Serial.println(data[1]);
+  Serial.print("[2]=");
+  Serial.println(data[2]);
+  Serial.print("[3]=");
+  Serial.println(data[3]);
+  Serial.println("---");
+}
+
+void play(uint8_t data[]) {
+  const uint8_t track = data[0] + 1;
+  const uint8_t note = data[1];
+  bool pressed = data[2];
+  const uint8_t velocity = data[3];
+  if (pressed) {
+    MIDI.sendNoteOn(note, velocity, track);
+    delay(10);
+  } else {
+    MIDI.sendNoteOff(note, 0, track);
+  }
+}
+ 
+void loop(void) {
+   if (radio.available()) {
+      radio.read(data, sizeof data);
+        for (int i = 0; i < bufferSize; i += 1) {
+          //show(data);
+           play(data);
+      }
+   }
+}
---
+++ send/send.ino
@@ -0,0 +1,79 @@
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <SPI.h>
+#include <Adafruit_MPR121.h>
+#include <Wire.h>
+ 
+const int pinCE = 9;
+const int pinCSN = 10;
+RF24 radio(pinCE, pinCSN);
+#define DO3 48
+#define TOUCH 30
+#define RELEASE 5
+// Single radio pipe address for the 2 nodes to communicate.
+const uint64_t pipe[5] = {0xF0F0F0F0D2LL, 0xF0F0F0F0C3LL, 0xF0F0F0F0B4LL, 0xF0F0F0F0A5LL, 0xF0F0F0F096LL };
+const int head = 0;
+uint8_t data[4] = {head,DO3,0,0};
+Adafruit_MPR121 mpr121 = Adafruit_MPR121();
+uint8_t mpr121Address = 0x5A;
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
+
+#ifndef _BV
+#define _BV(bit) (1 << (bit)) 
+#endif
+
+void setup(void)
+{
+   Serial.begin(115200);
+   // Iniciar el sensor
+   if (!mpr121.begin(mpr121Address)) {
+      Serial.println("MPR121 no encontrado");
+      while (1);
+   }
+   Serial.println("MPR121 iniciado");
+
+  // slow down some of the MPR121 baseline filtering to avoid 
+  // filtering out slow hand movements
+  mpr121.writeRegister(MPR121_NHDF, 0x01); //noise half delta (falling)
+  mpr121.writeRegister(MPR121_FDLF, 0x3F); //filter delay limit (falling)
+  mpr121.setThresholds(TOUCH, RELEASE);
+
+  //start the radio
+  radio.begin();
+  radio.openWritingPipe(pipe[head]);
+}
+ 
+void loop(void)
+{
+   currtouched = mpr121.touched();
+   for (int i = 0; i < 12; i += 1){
+     data[1] = DO3+i;
+     if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
+      data[2] = 1;
+      data[3] = map(mpr121.filteredData(i)-mpr121.baselineData(i),RELEASE,2*TOUCH,0,127);
+      show(data);
+      radio.write(data, sizeof data); 
+    }
+    if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
+      data[2] = 0;
+      data[3] = map(mpr121.filteredData(i)-mpr121.baselineData(i),RELEASE,2*TOUCH,0,127);
+      show(data);
+      radio.write(data, sizeof data); 
+    }
+   }
+   lasttouched = currtouched;
+}
+
+void show(uint8_t data[]){
+      Serial.print("header: ");
+      Serial.print(data[0]);
+      Serial.print("  note: ");
+      Serial.print(data[1]);
+      Serial.print("  keypress: ");
+      Serial.print(data[2]);
+      Serial.print("  velocity: ");
+      Serial.println(data[3]);
+      
+}

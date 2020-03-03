9e70f9ad9d7d2d2b989db7b48691008791f0decc
Carlos <cafubu@gmail.com>
2020-03-03 01:28:36 +0100

    Potentially working rock
    All in one program with configuration file

--- rock/rock.ino
+++ rock/rock.ino
@@ -7,25 +7,90 @@
 #include "thisrock.h"
 #include <SPI.h>
 
+//Radio instance and variables
+RF24 radio(PINCE, PINCS);
+uint8_t data[BUFF];
 
-//Configure radio parameters
-RF24 radio(pinCE, pinCS);
-const uint8_t bufferSize = 5;
-uint8_t data[bufferSize];
-#define DO3 48
+//Sensor instance and variables
+MPR121 cap = MPR121();
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
 
-//configure the sensor parameters
-MPR121 mpr121 = MPR121();
-
-//configure the MIDI parameters
+//Midi instance
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 void setup(void){
   radio.begin();
+  NRFconfig();
   MIDI.begin();
+  cap.begin(I2CADDR);
+  MPRconfig();
   Serial.begin(115200);
+  data[0] = HEAD;
+}
+
+void MPRconfig(){
+  cap.writeRegister(MPR121_NHDF, 0x01); //noise half delta (falling)
+  cap.writeRegister(MPR121_FDLF, 0x3F); //filter delay limit (falling)
+  cap.setThresholds(TOUCH, RELEASE);
+}
+
+void NRFconfig(void){
+  const uint64_t pipes[]=PIPES;
+  if(ROLE == 0){
+    for(int i = 0; i < 5; i += 1){
+      radio.openReadingPipe(i+1,pipes[i]);
+    }
+    radio.startListening();
+  }
+  else{
+    radio.openWritingPipe(pipes[HEAD]);
+  }
+}
+
+void play() {
+  const uint8_t channel = data[0] + 1;
+  const uint8_t note = data[1];
+  bool pressed = data[2];
+  const uint8_t velocity = data[3];
+  if (pressed) {
+    MIDI.sendNoteOn(note, velocity, channel);
+  } else {
+    MIDI.sendNoteOff(note, velocity, channel);
+  }
+  if(ROLE==1){
+    radio.write(data, sizeof data);
+  }
+}
+void self_check(int n) {
+  if ((currtouched & _BV(n)) && !(lasttouched & _BV(n)) ) {
+      data[1] = BASENOTE+n;
+      data[2] = 1;
+      data[3] = cap.filteredData(n)-cap.baselineData(n);
+      play();
+  }
+  if (!(currtouched & _BV(n)) && (lasttouched & _BV(n)) ) {
+      data[1] = BASENOTE+n;
+      data[2] = 0;
+      data[3] = cap.filteredData(n)-cap.baselineData(n);
+      play();
+  }
+}
+
+void receive() {
+  for(int n; n<5; n++){
+    if (radio.available()) {
+      radio.read(data, sizeof data);
+      play();
+    }
+  }
 }
 
 void loop(void){
-  
+  currtouched = cap.touched();
+   for (int i = 0; i < 12; i += 1){
+    self_check(i);
+    if(ROLE == 0){receive();}
+   }
+   lasttouched = currtouched;
 }
--- rock/thisrock.h
+++ rock/thisrock.h
@@ -1,21 +1,24 @@
 //Touch this
 //
 //Communications
-#define role 0
-#define head 0
+#define ROLE 0			//<-- 0 for receiver, 1 for transmitter
+#define HEAD 5			//<-- 5 for receiver, 0 to 4 for transmitter
 //Thresholds
-#define touch 30
-#define release 5
+#define TOUCH 30		//<-- optimize for plant
+#define RELEASE 5		//<-- optimize for plant
 
 
 //Do NOT touch this
 //
 //Sensor stuf
-#define I2Caddr 0x05
-
+#define I2CADDR 0x05
+#ifndef _BV
+#define _BV(bit) (1 << (bit)) 
+#endif
 //Radio stuff
-#define pipes 0xF0F0F0F0D2LL  0xF0F0F0F0C3LL 0xF0F0F0F0B4LL 0xF0F0F0F0A5LL 0xF0F0F0F096LL
-#define pinCE 9
-#define pinCS 10
+#define PIPES {0xF0F0F0F0D2LL,  0xF0F0F0F0C3LL, 0xF0F0F0F0B4LL, 0xF0F0F0F0A5LL, 0xF0F0F0F096LL}
+#define PINCE 9
+#define PINCS 10
+#define BUFF 5
 //Midi stuff
-#define DO3 48
+#define BASENOTE 48

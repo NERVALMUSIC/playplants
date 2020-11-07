2245e814084ce4736f66e056765789325bf1d021
carlos <cafubu@gmail.com>
2020-11-07 12:42:18 +0100

    New library, better sound

--- rock/rock.ino
+++ rock/rock.ino
@@ -5,14 +5,16 @@
 #include "thisrock.h"
 #include <SPI.h>
 
-#define DEBUG //Uncomment for debug
+
 //Radio instance and variables
 RF24 radio(PINCE, PINCS);
 uint8_t data[BUFF]; //data [0] is the channel - data[1] is hte note - data[2] es the message type - data[3] is the velocity
+
 //Sensor instance and variables
-MPR121 cap = MPR121();
-uint16_t lasttouched = 0;
-uint16_t currtouched = 0;
+int16_t maxdiff[SENSORS];
+int16_t mindiff[SENSORS];
+int16_t filteredCC[SENSORS];
+uint8_t windowCount = 1;
 
 //Midi instance
 #ifdef CONTROL
@@ -20,11 +22,16 @@ uint16_t currtouched = 0;
 #endif
 
 void setup(void){     //call to all setup functions and start serial port
-  cap.begin(I2CADDR);
+  MPR121.begin(I2CADDR);
   MPRconfig();
   radio.begin();
   NRFconfig();
   Serial.begin(115200);
+#ifdef CONTROL
+  Serial.println("Control");
+#else
+  Serial.println("Sender");
+#endif
 }
 
 void MPRconfig(){
@@ -34,24 +41,131 @@ void MPRconfig(){
    * AN3889: https://www.nxp.com/docs/en/application-note/AN3889.pdf
    * AN3890: https://www.nxp.com/docs/en/application-note/AN3890.pdf
    */
-  cap.writeRegister(MPR121_NHDF, 0x1); //noise half delta (falling)
-  cap.writeRegister(MPR121_FDLF, 0x3f); //filter delay limit (falling)
-  cap.setThresholds(TOUCH, RELEASE);    // Limits for Note ON and Note OFF signal send
+   
+    MPR121.goFast();      //I2C speed
+    MPR121.setTouchThreshold(TOUCH);      //Set Thresholds
+    MPR121.setReleaseThreshold(RELEASE);  //Set Thresholds
+  /*
+  * Optional settings, test best configuration for each plant,
+  * see datasheet and application notes to better understand how
+  * to change these settings.
+  */
+  
+    MPR121.setProxMode(PROX_0_11);
+    delay(500);
+    MPR121.autoSetElectrodes(false);  // autoset all electrode settings    
+
+
 }
 
 void NRFconfig(void){   //Radio stuff don't look
-  radio.setChannel(111);
+  radio.setChannel(120);
+  radio.setPALevel(RF24_PA_LOW);
   #ifdef CONTROL
-    for(int i = 0; i < 5; i += 1){
+    for(int i = 0; i < 4; i += 1){
       radio.openReadingPipe(i+1,PIPES[i]);
     }
     radio.startListening();
   #else
-    radio.openWritingPipe(PIPES[HEAD]);
+    radio.openWritingPipe(PIPES[HEAD-1]);
   #endif
 }
 
-void sendMIDI(uint8_t messageType, uint8_t channel, uint8_t data1, uint8_t data2) {
+void debug(){
+  #ifdef DEBUG
+  Serial.print("note");Serial.print("\t|");Serial.print("message");Serial.print("\t|");Serial.println("Velocity");
+  Serial.print(data[1]);Serial.print("\t|");Serial.print(data[2],HEX);Serial.print("\t|");Serial.println(data[3]);
+  #endif
+}
+
+
+#ifndef CONTROL   // Transmiter functions
+  void sendRadio() {
+    data[0] = HEAD;     //envia el canal (Editar thisrock)
+    MPR121.updateAll();
+    for(uint8_t n=0; n<SENSORS; n++){
+      int16_t diff = MPR121.getFilteredData(n)-MPR121.getBaselineData(n);
+      if( diff > maxdiff[n]){maxdiff[n] = diff;}
+      else if( diff < mindiff[n]){mindiff[n] = diff;}
+      data[1] = n; //envia la nota (Editar thisrock)
+      data[2] = CC;
+      data[3] = map(diff,maxdiff[n],mindiff[n],0,127);
+      if (n == 12 ){
+        if(data[3] >=20){ 
+          radio.write(data, sizeof data);
+          debug();
+        }
+      }
+      else if (MPR121.isNewTouch(n) ) {     //check if there is a new touch on the electrode
+        data[2] = NOTE_ON;
+        radio.write(data, sizeof data);
+        debug();
+      }
+      else if (MPR121.isNewRelease(n)) {   //check if the touch has just been lifted
+        data[2] = NOTE_OFF;
+        radio.write(data, sizeof data);
+        debug();
+      }
+      else if(MPR121.getTouchData(n)){
+        //filteredCC[n] += data[3];
+        //windowCount += 1;
+        //if(windowCount == WINDOW){
+        //  data[3] =filteredCC[n] / WINDOW;
+          radio.write(data, sizeof data);
+          debug();
+        //  windowCount == 1;
+        //}
+      }
+    }
+  }
+
+#else   //Receiver funcions
+
+  void setMode() {
+    for(uint8_t n=0; n < sizeof(ROCKS); n++){
+       MPR121.updateTouchData();
+       if(MPR121.getTouchData(ROCKS[n])){
+         for(uint8_t m=0; m < sizeof(MODES); m++){
+           if(MPR121.getTouchData(MODES[m])){
+             control[n] = m;
+           }
+         }
+       while(!MPR121.isNewRelease(ROCKS[n])){MPR121.updateTouchData();}       
+        sendMIDI(CC, HEAD, 20+n, control[n]);     //Send data as received (default mode)   
+       }
+     }
+  }
+  
+  void playMusic() {
+    switch (control[data[0]-1])   //choose mode for the channel received based on last time it was configured
+    {
+      case 0:
+      if( data[1] != 12 && (data[2] == NOTE_ON || data[2] == NOTE_OFF)){
+        sendMIDI(data[2], data[0], data[1], data[3]);     //Send data as received (default mode)
+      }
+      break;
+      case 1:
+      if( data[2] == CC && data[1] != 12){
+        sendMIDI(data[2], data[0], NOTES[data[0]][data[1]], data[3]);     //Send data as received (default mode)
+      }
+      break;
+      case 2:
+      if( data[1] == 12){
+        sendMIDI(data[2], data[0], NOTES2[data[0]][data[1]], data[3]);     //Send data as received (default mode)
+      }
+      break;
+      case 3:
+      if( data[2] == CC){
+        sendMIDI(data[2], data[0], NOTES2[data[0]][data[1]], data[3]);     //Send data as received (default mode)
+      }
+      break;
+      default:
+      sendMIDI(data[2], data[0], NOTES[data[0]][data[1]], data[3]);     //Send data as received (default mode)
+      break;
+    }
+  }
+
+  void sendMIDI(uint8_t messageType, uint8_t channel, uint8_t data1, uint8_t data2) {
                                                // corresponds to binary channel 0
   messageType &= 0b11110000;                   // Make sure that only the high nibble 
                                                // of the message type is set
@@ -66,111 +180,34 @@ void sendMIDI(uint8_t messageType, uint8_t channel, uint8_t data1, uint8_t data2
   data2       &= 0b01111111;
   #ifdef DEBUG
   Serial.print("Channel");Serial.print("\t|");Serial.print("Message");Serial.print("\t|");Serial.print("Byte 1");Serial.print("\t|");Serial.println("Byte 2");
-  Serial.print(channel);Serial.print("\t|");Serial.print(messageType);Serial.print("\t\t|");Serial.print(data1);Serial.print("\t|");Serial.println(data2);
+  Serial.print(channel);Serial.print("\t|");Serial.print(messageType,HEX);Serial.print("\t\t|");Serial.print(data1);Serial.print("\t|");Serial.println(data2);
   #else
   Serial.write(statusByte);                    // Send over Serial
   Serial.write(data1);
   Serial.write(data2);
   #endif
 }
-
-#ifndef CONTROL   // Transmiter code
-  void sendRadio(int n) {
-    data[0] = HEAD;     //envia el canal (Editar thisrock)
-    data[1] = n; //envia la nota (Editar thisrock)
-    data[3] = map(constrain(cap.baselineData(n)-cap.filteredData(n),0,300),0,300,0,127);      //get filtered capacitance (puede mejorarse)
-    if ((currtouched & _BV(n)) && !(lasttouched & _BV(n)) ) {     //check if there is a new touch on the electrode
-      data[2] = NOTE_ON;
-    }
-    else if (!(currtouched & _BV(n)) && (lasttouched & _BV(n))) {   //check if the touch has just been lifted
-      data[2] = NOTE_OFF;
-      radio.write(data, sizeof data);
-    }
-    else{
-      data[2] = CC;     //use the Control Change message in this case to be used for special purposes
-    }
-    //if(currtouched & _BV(n)){      // Only send data if someone is touching the electrode
-    //  radio.write(data, sizeof data);
-    //}
-    radio.write(data, sizeof data);
-  #ifdef DEBUG
-    Serial.print("Canal | ");Serial.print("Mensaje | ");Serial.print("Nota | ");Serial.println("Velocity");
-    Serial.print(data[0]);Serial.print("\t|\t");Serial.print(data[2]);Serial.print("\t|\t");Serial.print(data[1]);Serial.print("\t|\t");Serial.println(data[3]);
-  #endif
-  }
-
-#else   //Receiver code
-  uint8_t checkModes() {
-    uint8_t b = 0;
-      for (int n = 0; n < sizeof(MODES); n += 1)    //Loop through Mode selection electrodes
-      {
-        if (currtouched & _BV(MODES[n]))    //only check if the electrode is currently being touched, not if it is a new touch
-        {
-          bitWrite(b, n, 1);  //Set bit for each electorde that has a new touch, the table is as follows right now, more modes can be added:
-          /*                  mode 2  | mode 1  | mode 0  | Value
-           *                 ---------|---------|---------|--------
-           *                     0    |    0    |    0    |    0
-           *                     0    |    0    |    1    |    1
-           *                     0    |    1    |    0    |    2
-           *                     0    |    1    |    1    |    3
-           *                     1    |    0    |    0    |    4
-           *                     1    |    0    |    1    |    5
-           */
-        }
-      }
-    return b;
-  }
+  
 #endif
+
+
 void loop(void){
-  currtouched = cap.touched();    //get current Touch value for all electrodes
   #ifndef CONTROL   // Transmiter code
-    for (int i = 0; i < 12; i += 1){sendRadio(i);} // loop through electrodes
+    sendRadio();
   #else   //Receiver code
-    for (int i = 0; i < sizeof(ROCKS); i += 1) //loop through electrodes defined as rock selectors
-    {
-      if ((currtouched & _BV(ROCKS[i])) && !(lasttouched & _BV(ROCKS[i])))
-      {
-        control[i]=checkModes(); //get mode cobination value for recently touched rock selection electrode
-        #ifdef DEBUG
-          Serial.print("channel: ");Serial.println(i);
-          Serial.print("mode: ");Serial.println(control[i]);
-        #endif
-      }
+  uint8_t c = 0;
+  while(1){
+    if (c == 100){
+      setMode();
+      c = 0;
     }
-    if (radio.available()) {    //when new data is available read it and send it through midi
+    if (radio.available()){
       radio.read(data, sizeof data);    //load buffer
-      switch (control[data[0]])   //choose mode for the channel received based on last time it was configured
-      {
-        case 0:
-          if( data[2] == NOTE_ON || data[2] == NOTE_OFF){
-            sendMIDI(data[2], data[0], NOTES[data[0]][data[1]], data[3]);     //Send data as received (default mode)
-          }
-        break;
-        case 1:
-          if( data[2] == CC){
-            sendMIDI(data[2], data[0], NOTES[data[0]][data[1]], data[3]);     //Send data as received (default mode)
-          }
-        break;
-        case 2:
-          sendMIDI(CC, data[0], data[1], data[3]);   //Send Control changes only
-        break;
-        case 3:
-          sendMIDI(data[2], data[0], data[1], 127);   //Send data at maximum velocity when touched
-        break;
-        case 4:
-          sendMIDI(data[2], data[0], NOTES[map(data[3],50,120,0,sizeof(NOTES))], data[3]);   //Send NOTES depending on intensity
-        break;
-        case 5:
-            if( data[2] == NOTE_ON){
-              sendMIDI(data[2], data[0], data[1], data[3]);     //Send notes with sostenuto
-              sendMIDI(CC, data[0], 66, 127);   //Add sostenuto
-            }
-            else{sendMIDI(data[2], data[0], data[1], data[3]);}     //Send NoteOFF and CC data
-        break;
-        default:
-        break;
-      }
+      //debug();
+      playMusic();
     }
-  #endif
-   lasttouched = currtouched;     //Store last value to compare and get new touches and releases
+    c += 1; 
+  }
+
+  #endif     //Store last value to compare and get new touches and releases
 }
--- rock/thisrock.h
+++ rock/thisrock.h
@@ -1,27 +1,29 @@
 //Touch this
 //
 
-//Communications
-// #define CONTROL true	// <-- Comentar para emisor
+//Defines
+#define CONTROL true	// <-- Comentar para emisor
+#define DEBUG //Uncomment for debug
 
+//Sensor Stuff
 #ifndef CONTROL
-  #define HEAD 0			  // <-- Hardcode value between 1 and 5
+  #define HEAD 1			  // <-- Hardcode value between 1 and 4
+  #define TOUCH 20   //<-- optimize for PLANT
+  #define RELEASE 2   //<-- optimize for PLANT
+#else
+  #define HEAD 10
+  #define TOUCH 20   //<-- optimize for PLANT
+  #define RELEASE 10   //<-- optimize for PLANT
 #endif
 
-//Thresholds
-#define TOUCH 20		//<-- optimize for CONTROL
-#define RELEASE 5		//<-- optimize for CONTROL
-//
-//#define TOUCH 30    //<-- optimize for PLANT
-//#define RELEASE 5   //<-- optimize for PLANT
-
 //Midi stuff
-const uint8_t NOTES[ 4 ][ 12 ] = { {50,53,57,60,62,65,69,72,74,77,81,84}, {60,63,67,70,72,75,79,82,84,87,91,94} , {70,73,77,80,82,85,89,92,94,97,101,104}, {60,63,67,70,72,75,79,82,84,87,91,94} };
+const uint8_t NOTES[ 4 ][ 12 ] = { {50, 53, 57, 60, 62, 65, 69, 72, 74, 77, 81, 84}, {60, 63, 67, 70, 72, 75, 79, 82, 84, 87, 91, 94} , {70, 73, 77, 80, 82, 85, 89, 92, 94, 97, 101, 104}, {60, 63, 67, 70, 72, 75, 79, 82, 84, 87, 91, 94} };
+const uint8_t NOTES2[ 4 ][ 12 ] = { {50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50}, {60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60} , {70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70}, {80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80} };
 //<-- notes to play for this rock (set also for receiver in case it needs to be used)
-  
+
 #ifdef CONTROL
-  const uint8_t MODES[3]{0,1,2};        //<-- Electrodes used for mode control (3 needed) up to 6 can be used
-  const uint8_t ROCKS[5]{8,9,10,11};  //<-- Electrodes used for rock selection (5 maximum rocks)
+const uint8_t MODES[4] {0, 3, 4, 6};      //<-- Electrodes used for mode control (3 needed) up to 6 can be used
+const uint8_t ROCKS[4] {1, 2, 5, 7}; //<-- Electrodes used for rock selection (4 maximum rocks)
 #endif
 const uint8_t NOTE_OFF = 0x80;          //<-- Used Midi message types
 const uint8_t NOTE_ON = 0x90;           //<-- Used Midi message types
@@ -36,12 +38,11 @@ const uint8_t PITCH_BEND = 0xE0;        //<-- Used Midi message types
 
 //Sensor stuf
 #define I2CADDR 0x5A
-#ifndef _BV
-#define _BV(bit) (1 << (bit))
-#endif
+#define SENSORS 13
+#define WINDOW 100
 
 //Radio stuff
-const uint64_t PIPES[5] {0xF0F0F0F0D2LL,  0xF0F0F0F0C3LL, 0xF0F0F0F0B4LL, 0xF0F0F0F0A5LL, 0xF0F0F0F096LL};
+const uint64_t PIPES[4] {0xF0F0F0F0D2LL,  0xF0F0F0F0C3LL, 0xF0F0F0F0B4LL, 0xF0F0F0F0A5LL};
 #define PINCE 9
 #define PINCS 10
 #define BUFF 4

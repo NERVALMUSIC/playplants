a184b295f1a7c1ad07c8f02efb1880183e4d4fc2
carlos-ekuore <carlos.delafuente@ekuore.com>
2021-12-27 17:40:13 +0100

    Sensor and BLE uart
    Sensor working
    Added BLE debug capabilities

--- FW/planter/planter.ino
+++ FW/planter/planter.ino
@@ -1,22 +1,28 @@
 #include "src/nRF5x_lowPower/Arduino_nRF5x_lowPower.h"
 #include "src/OneButton/OneButton.h"
 #include "src/RGBLed/RGBLed.h"
-#include "src/AT42QT/AT42QT.h"
+#include "src/AT42QT/AT42QT2120.h"
 #include "nrf52.h"
+#include "src/BLESerial/BLESerial.h"
 #include <BLEPeripheral.h>
 #include "thisplanter.h"
 
 void setup()
-{
+{  
+  //Setup led
+  led.off();
   //Setup user button
   but.attachClick(singleclick);
   but.attachDoubleClick(doubleclick);
   but.attachLongPressStop(longPressStop);
   nRF5x_lowPower.enableWakeupByInterrupt(BUTTON_PIN, FALLING);
-
-  //Setup Sensor
-
+  
   //Setup radio
+#ifdef DEBUG
+  bleSerial.setLocalName("ATTEC-DEBUG");
+  bleSerial.setDeviceName("ATTEC-DEBUG");           //device name sometimes used by central
+  bleSerial.begin();
+#else
   blePeripheral.setLocalName("ATEEC PLANTER");            //local name sometimes used by central
   blePeripheral.setDeviceName("ATEEC PLANTER");           //device name sometimes used by central
   blePeripheral.setAppearance(0x0340);                    //default is 0x0000, Check Bluetooth spec
@@ -27,9 +33,14 @@ void setup()
   characteristic.setValue(0);                             //Set initial value¡
   blePeripheral.begin();                                  //Initialize de periferal
   BLECentral central = blePeripheral.central();           //Start Advertising
+#endif
 
-  
-  //Setup led
+  //Setup Sensor
+  touch_sensor.begin();  
+  touch_sensor.reset();
+  delay(RESET_DELAY);
+  touch_sensor.triggerCalibration();
+  delay(CALIBRATION_LOOP_DELAY);
   led.fadeIn(RGBLed::WHITE, 100, 1000); // Fade in with 5 steps during 100ms
   led.fadeOut(RGBLed::WHITE, 100, 1000); // Fade in with 5 steps during 100ms
 }
@@ -44,34 +55,59 @@ void loop()
   }
   Sensormanager();
   BLEmanager();
+  //Batterymanager();
 }
 
 void BLEmanager()
 {
+#ifdef DEBUG
+  bleSerial.poll();
+  if (bleSerial)
+  {
+    led.setColor(RGBLed::BLUE);
+    for (uint8_t key=0; key < touch_sensor.KEY_COUNT; ++key)
+    {
+      if(touch_sensor.touched(plantstatus,key)){
+        bleSerial.print("Sensor "); bleSerial.print(key);
+        bleSerial.println(" Touched");
+        led.flash(RGBLed::GREEN, 5);
+      }
+    }
+  }
+  else
+  {
+    led.flash(RGBLed::BLUE, 200); // Light on during 400ms and flash with interval (200ms)
+  }
+#else
 
+#endif
 }
 
 //Poll the sensor inputs and convert any relevant data to midi format
 void Sensormanager()
 {
-
+  if (touch_sensor.communicating())
+  {
+    plantstatus = touch_sensor.getStatus();
+  }
 }
 
 // This function will be called when the button is pressed once.
 void singleclick() {
+  if (bleSerial) {bleSerial.println("Single click");}
   led.flash(RGBLed::GREEN, 200); // Light on during 400ms and flash with interval (200ms)
 } // singleclick
 
-
 // This function will be called when the button was pressed 2 times in a short timeframe.
 void doubleclick() {
+  if (bleSerial) {bleSerial.println("Double click");}
   led.flash(RGBLed::GREEN, 200, 200); // Light on during 400ms and flash with interval (200ms)
   led.flash(RGBLed::GREEN, 200, 200); // Light on during 400ms and flash with interval (200ms)
 } // doubleclick
 
-
 // This function will be called once, when the button is released after beeing pressed for a long time.
 void longPressStop() {
+  if (bleSerial) {bleSerial.println("Long click");}
   sleep = true;
   led.fadeIn(RGBLed::RED, 100, 1000); // Fade in with 5 steps during 100ms
   led.fadeOut(RGBLed::RED, 100, 1000); // Fade in with 5 steps during 100ms
--- FW/planter/thisplanter.h
+++ FW/planter/thisplanter.h
@@ -1,6 +1,6 @@
 //Touch this
 //
-
+#define DEBUG
 //MIDI Stuff
 
 const uint8_t NOTE_OFF = 0x80;          //<-- Used Midi message types
@@ -14,29 +14,43 @@ const uint8_t CC = 0xB0;                //<-- Used Midi message types
 //Do NOT touch this
 //
 
-//Sensor stuf
-
-
 //Radio stuff
-BLEPeripheral blePeripheral;
-BLEService service("03B80E5A-EDE8-4B33-A751-6CE34EC4C700");
-BLECharacteristic characteristic("7772E5DB-3868-4112-A1A9-F2669D106BF3", BLERead | BLEWriteWithoutResponse | BLENotify, 20 );
-BLEDescriptor descriptor = BLEDescriptor("2902", 0);
+#ifdef DEBUG
+  BLESerial bleSerial;
+#else
+  BLEPeripheral blePeripheral;
+  BLEService service("03B80E5A-EDE8-4B33-A751-6CE34EC4C700");
+  BLECharacteristic characteristic("7772E5DB-3868-4112-A1A9-F2669D106BF3", BLERead | BLEWriteWithoutResponse | BLENotify, 20 );
+  BLEDescriptor descriptor = BLEDescriptor("2902", 0);
+#endif
+
 unsigned long msOffset = 0;
 #define MAX_MS 0x01FFF //13 bits, 8192 dec
 
+//HW stuff
+#define RED_LED         16    // RGB LED R
+#define GRN_LED         14    // RGB LED G
+#define BLU_LED         15    // RGB LED B
+#define CHG             03    // Charge indication
+#define BUTTON_PIN      04    // User button
+#define POW_EN          05    // Enable battery readings
+#define INT             06    // Sensor Interrupt pin
+int bat_mon = A0;             // Analog input from battery
+#define SDA             00    // SDA for sensor
+#define SCL             01    // SCL for sensor
+
 //User Interface
-#define RED_LED       	16  	// RGB LED R
-#define GRN_LED       	14  	// RGB LED G
-#define BLU_LED       	15  	// RGB LED B
 RGBLed led(RED_LED, GRN_LED, BLU_LED, RGBLed::COMMON_ANODE);
-
-
-#define BUTTON_PIN      04  	// User button
 OneButton but(
   BUTTON_PIN,  // Input pin for the button
   true,        // Button is active LOW
   true         // Enable internal pull-up resistor
 );
-//HW stuff
 static bool sleep = false;
+
+
+//Sensor stuf
+AT42QT2120 touch_sensor;
+AT42QT2120::Status plantstatus;
+const int RESET_DELAY = 2000;
+const int CALIBRATION_LOOP_DELAY = 50;
